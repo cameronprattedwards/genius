99bcdac04bdbf937dd757021a927a8d774faaa70
Cameron Edwards <cameronprattedwards@gmail.com>
2014-01-14 15:17:56 -0500

    Add Genius Angular with gnModel directive.

---
+++ angular-genius.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    var app = angular.module("Genius", []);
+    app.directive("gnModel", function () {
+        return {
+            scope: {
+                gnModel: "="
+            },
+            link: function (scope, elem, attrs) {
+                elem.val(scope.gnModel());
+                elem.bind("keyup", function () {
+                    scope.gnModel(elem.val());
+                    scope.$apply();
+                });
+            }
+        };
+    });
+}());
