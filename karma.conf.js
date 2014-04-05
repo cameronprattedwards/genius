2e7481b33514e1ee31eafa5ccfa637e15b1b7e21
Cameron Edwards <cameronprattedwards@gmail.com>
2014-04-05 09:52:47 -0400

    Move test files into test folder

--- karma.conf.js
+++ karma.conf.js
@@ -14,8 +14,8 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'genius.js',
-      'genius-base-spec.js',
-      'genius-spec.js'
+      'test/genius-base-spec.js',
+      'test/genius-spec.js'
     ],
 
 
