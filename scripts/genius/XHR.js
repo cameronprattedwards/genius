f0c0fc26568d5a9d9f96ce346bd632e6118e4d95
Cameron Edwards <cameronprattedwards@gmail.com>
2014-04-09 18:59:58 -0400

    Tear out old code

--- scripts/genius/XHR.js
+++
@@ -1,7 +0,0 @@
-define(["./box"], function (box) {
-    if (window.XMLHttpRequest) {
-        box.set("XHR", function () { return new XMLHttpRequest(); });
-    } else if (window.ActiveXObject) {
-        box.set("XHR", function () { return new ActiveXObject("Microsoft.XMLHTTP"); });
-    }
-});
