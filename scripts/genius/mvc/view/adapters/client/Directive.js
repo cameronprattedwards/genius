44acd4775b3b4382afbb746cd82cd045809a440a
Cameron Edwards <cameronprattedwards@gmail.com>
2014-04-23 08:20:29 -0400

    Compartmentalize compile boilerplate in directives

--- scripts/genius/mvc/view/adapters/client/Directive.js
+++ scripts/genius/mvc/view/adapters/client/Directive.js
@@ -1,4 +1,4 @@
-define(["./Node"], function (Node) {
+define(["./Node", "./directives/splice"], function (Node, splice) {
 	return Node.extend({
 		init: function () {
 			this.name = "";
@@ -8,10 +8,14 @@ define(["./Node"], function (Node) {
 			this.open = null;
 			this.close = null;
 		},
+		splice: function (index, length) {
+			var offset = Array.prototype.indexOf.call(this.parent.childNodes, this.compiledOpen) + 1;
+			var args = Array.prototype.slice.call(arguments, 2);
+			return splice.apply(this, [index + offset, length].concat(args));
+		},
 		compile: function (model, parent) {
-			this.compiledChildren = this.children.map(function (child) {
-				return child.compile(model, parent);
-			});
+			this.parent = parent;
+			var _self = this;
 
 			var args = [parent, model];
 			for (var i = 0; i < this.args.length; i++) {
@@ -20,12 +24,27 @@ define(["./Node"], function (Node) {
 				}
 			}
 
-			this.setUp.apply(this, args);
+			var output = this.setUp.apply(this, args);
+
+			for (var i = 0; i < args.length; i++)
+				if (args[i].subscribe)
+					args[i].subscribe(function () { _self.update.apply(_self, args); });
+
+			this.compiledOpen = this.open.compile(model)[0];
+			this.compiledClose = this.close.compile(model)[0];
+
+			output.unshift(this.compiledOpen);
+			output.push(this.compiledClose);
+
+			return output;
+		},
+		setUp: function (parent, model) {
+			this.compiledChildren = this.children.map(function (child) {
+				return child.compile(model, parent);
+			});
 
 			return this.compiledChildren;
 		},
-		setUp: function () {
-
-		}
+		update: function () {}
 	});
 });
