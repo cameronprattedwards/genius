7b0cc0d64c004a08a29c50ecefa5a947671b7cca
Cameron Edwards <cameronprattedwards@gmail.com>
2014-04-23 08:20:57 -0400

    Make Each and If comply with refactored directives

--- scripts/genius/mvc/view/adapters/client/directives/Each.js
+++ scripts/genius/mvc/view/adapters/client/directives/Each.js
@@ -1,76 +1,57 @@
 define(["../Directive", "./splice"], function (Directive, splice) {
 	return Directive.extend({
-		setUp: function () {
-			
+		init: function () {
+			Directive.prototype.init.apply(this, arguments);
+
+			this.cached = [];
 		},
-		compile: function (model, parent) {
-			var indexOf = Array.prototype.indexOf.call,
-				cached = [],
-				_self = this;
-
-			model = model[this.args[0]];
-
-			function template(model) {
-				var output = [];
-				for (var i = 0; i < this.children.length; i++) {
-					output.push.apply(output, this.children[i].compile(model));
-				}
-				return output;
+		template: function (model) {
+			var output = [];
+			for (var i = 0; i < this.children.length; i++) {
+				output.push.apply(output, this.children[i].compile(model));
 			}
-
-			function domIndexFn(offset, i, kids) {
-				return offset + (i * kids.length);
-			}
-
-			function update(array) {
-				var i = 0,
-					offset = Array.prototype.indexOf.call(parent.childNodes, _self.compiledOpen) + 1,
-					domIndex = domIndexFn(offset, i, _self.children);
-
-				for (; i < array.length; i++) {
-					var entry = array[i],
-						entryInCached,
-						entryEls,
-						domIndex = domIndexFn(offset, i, _self.children);
-
-					if (cached[i] === entry)
-						continue;
-
-					if (cached.indexOf(entry) !== -1) {
-						while (cached.indexOf(entry) !== -1) {
-							var indexInCached = cached.indexOf(entry);
-							entryInCached = cached.splice(indexInCached, 1)[0];
-							entryEls = splice(parent, indexInCached + offset, _self.children.length);
-						}
-					} else {
-						entryInCached = entry;
-						entryEls = template.call(_self, entry);
-					}
-
-					cached.splice(i, 0, entryInCached);
-					splice.apply(parent, [parent, domIndex, 0].concat(entryEls));
-				}
-
-				var extraLength = cached.length - array.length;
-				cached.splice(i, extraLength);
-				splice(parent, domIndex, extraLength * _self.children.length);
-			}
-
-			model.subscribe(update);
-
-			var output = this.open.compile();
-			this.compiledOpen = output[0];
-
-			for (var i = 0; i < model.length; i++) {
-				cached.push(model[i]);
-				output.push.apply(output, template.call(this, model[i]));
-			}
-
-			var compiledCloseArray = this.close.compile();
-			this.compiledClose = compiledCloseArray[0];
-			output.push(this.compiledClose);
-
 			return output;
+		},
+		setUp: function (model, parent, array) {
+			var output = [];
+			for (var i = 0; i < array.length; i++) {
+				this.cached.push(array[i]);
+				output.push.apply(output, this.template.call(this, array[i]));
+			}
+			return output;
+		},
+		update: function (model, parent, array) {
+			var i = 0,
+				domIndex = 0;
+
+			for (; i < array.length; i++) {
+				var entry = array[i],
+					entryInCached,
+					entryEls,
+					domIndex = i * this.children;
+
+				if (this.cached[i] === entry)
+					continue;
+
+				if (this.cached.indexOf(entry) !== -1) {
+					while (this.cached.indexOf(entry) !== -1) {
+						var indexInCached = this.cached.indexOf(entry);
+						entryInCached = this.cached.splice(indexInCached, 1)[0];
+						entryEls = this.splice(indexInCached, this.children.length);
+					}
+				} else {
+					entryInCached = entry;
+					entryEls = this.template(entry);
+				}
+
+				this.cached.splice(i, 0, entryInCached);
+				this.splice.apply(this, [domIndex, 0].concat(entryEls));
+			}
+
+			var extraLength = this.cached.length - array.length;
+
+			this.cached.splice(i, extraLength);
+			this.splice(domIndex, extraLength * this.children.length);
 		}
 	});
 });
--- scripts/genius/mvc/view/adapters/client/directives/If.js
+++ scripts/genius/mvc/view/adapters/client/directives/If.js
@@ -1,4 +1,4 @@
-define(["../Directive", "./splice"], function (Directive, splice) {
+define(["../Directive"], function (Directive) {
 	return Directive.extend({
 		setUp: function (parent, model, conditional) {
 			function template(model) {
@@ -9,51 +9,16 @@ define(["../Directive", "./splice"], function (Directive, splice) {
 				return output;
 			}
 
-			var compiledChildren = template.call(this, model);
-		}
+			this.compiledChildren = template.call(this, model);
 
-		compile: function (model, parent) {
-			var _self = this,
-				conditional;
-
-			with (model) {
-				conditional = eval(_self.args[0]);
+			return conditional.get() ? this.compiledChildren : [];
+		},
+		update: function (parent, model, conditional) {
+			if (conditional.get()) {
+				this.splice.apply(this, [0, 0].concat(this.compiledChildren));
+			} else {
+				this.splice(0, 0, this.compiledChildren.length);
 			}
-
-			function template(model) {
-				var output = [];
-				for (var i = 0; i < this.children.length; i++) {
-					output.push.apply(output, this.children[i].compile(model));
-				}
-				return output;
-			}
-
-			var compiledChildren = template.call(this, model);
-
-			function update(value) {
-				var offset = Array.prototype.indexOf.call(parent.childNodes, _self.compiledOpen) + 1;
-
-				if (value) {
-					splice.apply(this, [parent, offset, 0].concat(compiledChildren));
-				} else {
-					splice(parent, offset, _self.children.length);
-				}
-			}
-
-			conditional.subscribe(update);
-
-			var output = this.open.compile(model);
-
-			this.compiledOpen = output[0];
-
-			if (conditional.get())
-				output.push.apply(output, compiledChildren);
-
-			var compiledCloseArray = this.close.compile(model);
-			this.compiledClose = compiledCloseArray[0];
-			output.push(this.compiledClose);
-
-			return output;
 		}
 	});
 });
