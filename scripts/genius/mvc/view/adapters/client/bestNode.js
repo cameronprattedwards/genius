a1843c222b5b5f106effaea8caa468fe8974973d
Cameron Edwards <cameronprattedwards@gmail.com>
2014-04-23 19:25:04 -0400

    Rename bestNode

--- scripts/genius/mvc/view/adapters/client/bestNode.js
+++ scripts/genius/mvc/view/adapters/client/bestNode.js
@@ -6,14 +6,14 @@ define([
 	], function (HTMLFactory, DirectiveFactory, TextFactory, directives) {
 	var bestNode = function (html) {
 		if (/^<[^>]+>/.test(html[0]))
-			return HTMLFactory(html, bestNode);
+			return require("./HTMLFactory")(html, bestNode);
 
 		if (/^\{\{#[^\}]+\}\}/.test(html[0])) {
 			var tagName = /\{\{#([^\s]+)[^\}]*\}\}/.exec(html)[1];
-			return DirectiveFactory(directives[tagName])(html, bestNode);
+			return require("./DirectiveFactory")(directives[tagName])(html, bestNode);
 		}
 
-		return TextFactory(html, bestNode);
+		return require("./TextFactory")(html, bestNode);
 	};
 
 	return bestNode;
