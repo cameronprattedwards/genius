6cbc51ae208c3f93b3f835173b9573cf2a36e2f1
Cameron Edwards <cameronprattedwards@gmail.com>
2014-04-23 12:42:54 -0400

    Fill out Template functionality

--- scripts/genius/mvc/view/adapters/client/directives/Template.js
+++ scripts/genius/mvc/view/adapters/client/directives/Template.js
@@ -1,23 +1,31 @@
-define(["../Directive", "./unwrap"], function (Directive, unwrap) {
-	function renderTemplate(model, parent, filePath, modelToPass) {
-		require(["../compile"], function (compile) {
-		});
-	}
-
+define(["../Directive", "./unwrap", "genius/utils"], function (Directive, unwrap, utils) {
 	return Directive.extend({
-		setUp: function () {},
-		update: function () {}
-		compile: function (model, parent) {
-			var filePath,
-				modelToPass,
-				_self = this;
+		setUp: function (model, parent, filePath, modelToPass) {
+			var _self = this;
+			// AJAX - Get the template
+			var promise = utils.ajax({
+				url: unwrap(filePath)
+			});
+			promise.success(function (html) {
+				//Run the template through a factory to get a pseudo-DOM
+				_self.children = PseudoDomFactory.create(html);
+				_self.compiledChildren = _self.children.reduce(function (prev, curr, index, array) { return prev.concat(curr.compile(modelToPass)) }, []);
 
-			with (model) {
-				filePath = eval(_self.args[0]);
-				modelToPass = eval(_self.args[1]);
-			}
-
-			
+				//Compile the pseudo-DOM into a DOM.
+				_self.splice.apply(_self, [0, 0].concat(_self.compiledChildren));
+			});
+			return [];
+		},
+		update: function (model, parent, filePath, modelToPass) {
+			//Unless a dependency fires before the AJAX request returns, we should have our pseudo-DOM ready to go already
+			this.splice(0, this.compiledChildren.length);
+			this.setUp.apply(this, arguments);
 		}
 	});
 });
+
+//Is there any way to do this without circular dependencies?
+//The core function:
+//AJAX-Get a template
+//Run the template through a factory to get a pseudo-DOM
+//Compile the pseudo-DOM into a DOM.
