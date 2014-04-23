f52ba8f9c114a4edc55b968aa102551855c3b219
Cameron Edwards <cameronprattedwards@gmail.com>
2014-04-23 19:27:20 -0400

    Add error handling to TemplateCache

--- scripts/genius/mvc/view/adapters/client/TemplateCache.js
+++ scripts/genius/mvc/view/adapters/client/TemplateCache.js
@@ -7,9 +7,14 @@ define(["genius/utils", "genius/utils/deferred"], function (utils, deferred) {
 			output.resolve(templates[url]);
 		} else {
 			var promise = utils.ajax({ url: url });
+
 			promise.success(function (html) {
 				templates[url] = PseudoDomFactory(html);
 				output.resolve(templates[url]);
+			});
+
+			promise.fail(function () {
+				output.reject.apply(output, arguments);
 			});
 		}
 		return output.promise();
