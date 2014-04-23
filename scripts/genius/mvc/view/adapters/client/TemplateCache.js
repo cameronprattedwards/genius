c95e3d87130ca90a844f8bd65098f50f8b9e206d
Cameron Edwards <cameronprattedwards@gmail.com>
2014-04-23 18:19:21 -0400

    Add TemplateCache class

---
+++ scripts/genius/mvc/view/adapters/client/TemplateCache.js
@@ -0,0 +1,17 @@
+define(["genius/utils", "genius/utils/deferred"], function (deferred) {
+	var templates = {};
+
+	return function (url) {
+		var output = deferred();
+		if (templates[url]) {
+			output.resolve(templates[url]);
+		} else {
+			var promise = utils.ajax({ url: url });
+			promise.success(function (html) {
+				templates[url] = PseudoDomFactory(html);
+				output.resolve(templates[url]);
+			});
+		}
+		return output.promise();
+	}
+});
