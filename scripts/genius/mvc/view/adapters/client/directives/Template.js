7098120514c5c4ff144e6e3f9cd8467768931027
Cameron Edwards <cameronprattedwards@gmail.com>
2014-04-23 23:24:46 -0400

    Fix require bug and splice bug

--- scripts/genius/mvc/view/adapters/client/directives/Template.js
+++ scripts/genius/mvc/view/adapters/client/directives/Template.js
@@ -1,12 +1,12 @@
-define(["../TemplateCache", "./unwrap", "../Directive"], function (TemplateCache, unwrap, Directive) {
+define(["../TemplateCache", "./unwrap", "../Directive", "require"], function (TemplateCache, unwrap, Directive, require) {
 	return Directive.extend({
 		setUp: function (model, parent, filePath, modelToPass) {
 			var _self = this;
 
-			TemplateCache(unwrap(filePath))
+			require("../TemplateCache")(unwrap(filePath))
 				.success(function (pseudoDom) {
 					_self.compiledChildren = pseudoDom.compile(modelToPass, parent);
-					_self.splice(0, 0, _self.compiledChildren);
+					_self.splice.apply(_self, [0, 0].concat(_self.compiledChildren));
 				});
 
 			return [];
