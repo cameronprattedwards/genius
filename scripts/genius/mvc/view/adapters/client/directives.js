2b8e217d60099deeacf8e1f331e56caff21b2739
Cameron Edwards <cameronprattedwards@gmail.com>
2014-04-23 22:07:54 -0400

    Register Template directive

--- scripts/genius/mvc/view/adapters/client/directives.js
+++ scripts/genius/mvc/view/adapters/client/directives.js
@@ -3,13 +3,17 @@ define([
 	"./directives/If", 
 	"./directives/Value", 
 	"./directives/With", 
-	"./directives/Event"
-	], function (Each, If, Value, With, Event) {
+	"./directives/Event",
+	"./directives/Template"
+	], function (Each, If, Value, With, Event, Template) {
+
 	return {
 		"if": If,
 		"each": Each,
 		"value": Value,
 		"with": With,
-		"event": Event
+		"event": Event,
+		"template": Template
 	};
+
 });
