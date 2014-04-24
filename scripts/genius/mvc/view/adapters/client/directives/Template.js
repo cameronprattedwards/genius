100e621d07a1e34fae57eff1f22fdb26a68cba68
Cameron Edwards <cameronprattedwards@gmail.com>
2014-04-23 21:53:13 -0400

    Fix Template syntax issue

--- scripts/genius/mvc/view/adapters/client/directives/Template.js
+++ scripts/genius/mvc/view/adapters/client/directives/Template.js
@@ -1,17 +1,19 @@
-define(["../TemplateCache", "./unwrap"], function (TemplateCache, unwrap) {
-	setUp: function (model, parent, filePath, modelToPass) {
-		var _self = this;
+define(["../TemplateCache", "./unwrap", "../Directive"], function (TemplateCache, unwrap, Directive) {
+	return Directive.extend({
+		setUp: function (model, parent, filePath, modelToPass) {
+			var _self = this;
 
-		TemplateCache(unwrap(filePath))
-			.success(function (pseudoDom) {
-				_self.compiledChildren = pseudoDom.compile(modelToPass, parent);
-				_self.splice(0, 0, _self.compiledChildren);
-			});
+			TemplateCache(unwrap(filePath))
+				.success(function (pseudoDom) {
+					_self.compiledChildren = pseudoDom.compile(modelToPass, parent);
+					_self.splice(0, 0, _self.compiledChildren);
+				});
 
-		return [];
-	},
-	update: function () {
-		this.splice(0, this.compiledChildren.length);
-		this.setUp.apply(this, arguments);
-	}
+			return [];
+		},
+		update: function () {
+			this.splice(0, this.compiledChildren.length);
+			this.setUp.apply(this, arguments);
+		}
+	});
 });
