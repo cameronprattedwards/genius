85e2b7bae160b2b8d214f9107e509e3acd9a0baf
Cameron Edwards <cameronprattedwards@gmail.com>
2014-04-24 20:00:33 -0400

    Make ToDo object API-ready

--- scripts/app/models/ToDo.js
+++ scripts/app/models/ToDo.js
@@ -1,7 +1,11 @@
 var root = "genius/mvc/model";
-define([root + "/resource/Resource", root + "/types/String", root + "/types/Date"], function (Resource, String, Date) {
+define([root + "/resource/Resource", root + "/types/String", root + "/types/Date", root + "/types/Number"], function (Resource, String, Date, Number) {
 	return Resource.extend({
 		text: String(),
-		dueDate: Date()
+		dueDate: Date(),
+		id: Number(),
+		url: function () { 
+			return "/api/todos/" + (this.id ? this.id.get() : "");
+		}
 	});
 });
