b6d5515cfcb59ed7c9cdf99b27139084f4f2c280
Cameron Edwards <cameronprattedwards@gmail.com>
2014-05-13 20:56:29 -0400

    Point Controller at /model directory

--- mvc/controller/adapters/client/Controller.js
+++ mvc/controller/adapters/client/Controller.js
@@ -1,4 +1,4 @@
-define(["genius/mvc/model/base/Class", "genius/mvc/view/adapters/client/compile"], function (Class, compile) {
+define(["genius/model/base/Class", "genius/mvc/view/adapters/client/compile"], function (Class, compile) {
 	return Class.extend({
 		render: function (filePath, model) {
 			compile(filePath, document.body, model);
