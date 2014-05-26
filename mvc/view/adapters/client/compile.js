9a16c5dd8578bb7744ada334b3a267523a5b20b4
Cameron Edwards <cameronprattedwards@gmail.com>
2014-05-26 16:51:23 -0400

    Add functionality for compiling templates

--- mvc/view/adapters/client/compile.js
+++ mvc/view/adapters/client/compile.js
@@ -1,5 +1,22 @@
-define([], function () {
-	return function () {
-	
+define(["genius/config", "genius/utils/ajax", "./factories/Document", "./Bind"], function (config, ajax, DocumentFactory, Bind) {
+	return function (template, model) {
+		var node = config.get().baseNode;
+		scope = {
+			dom: [node],
+			components: {},
+			model: [model]
+		};
+
+		ajax({url: template})
+			.success(function (html) {
+				console.log(html);
+				var doc = DocumentFactory.fromString(html, scope);
+				window.doc = doc;
+				Bind(node).to(doc);
+			})
+			.fail(function () {
+				console.log("fail", arguments);
+			});
+
 	};
 });
