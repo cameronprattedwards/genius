ea68b78e414c21edeaee0053f43ec634e0bbae82
Cameron Edwards <cameronprattedwards@gmail.com>
2014-05-26 16:52:13 -0400

    Add valueOf override

--- model/observable/Computed.js
+++ model/observable/Computed.js
@@ -7,6 +7,8 @@ define(["./Observable"], function (Observable) {
 
 			this.get = evaluator;
 
+			this.valueOf = evaluator;
+
 			for (var i = 0; i < dependencies.length; i++) {
 				dependencies[i].subscribe(function () { _self.fire.call(_self); });
 			}
